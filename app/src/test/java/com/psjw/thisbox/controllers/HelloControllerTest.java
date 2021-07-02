19e0505d4f957abb516f720b107e2ebfe74c3372
psjw <dlrudtn108@gmail.com>
2021-07-02 15:43:11 +0900

    최초 CI/CD 구성 (도커 허브 업로드까지)

---
+++ app/src/test/java/com/psjw/thisbox/controllers/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.psjw.thisbox.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+    HelloController controller = new HelloController();
+
+    @Test
+    public void hello(){
+        assertEquals("hello",controller.hello());
+    }
+}
